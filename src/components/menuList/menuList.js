d8616f1395468efe62c23263d0159a261e269c8f
eneax <19818937+eneax@users.noreply.github.com>
2019-07-22 17:48:50 +0200

    Restart MenuList

--- src/components/menuList/menuList.js
+++ src/components/menuList/menuList.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import './menuList.scss'
+import MenuItem from '../menuItem/menuItem'
 
-const MenuList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class MenuList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        },
+        {
+          id: 2,
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        },
+        {
+          id: 3,
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        },
+        {
+          id: 4,
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        },
+        {
+          id: 5,
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="menuList">
+        {
+          this.state.sections.map(({id, title, imageUrl}) => (
+            <MenuItem 
+              key={id}
+              title={title}
+            />
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default MenuList
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,44 +1,10 @@
 import React from 'react'
 import './home.scss'
+import MenuList from '../../components/menuList/menuList'
 
 const Home = () => (
   <div className='homepage'>
-    <div className="menuList">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-      
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-    </div>
+    <MenuList />
   </div>
 )
 
