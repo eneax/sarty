88766ec2ae4875f7dce2da10f64a0856f9b60707
eneax <19818937+eneax@users.noreply.github.com>
2019-07-22 18:21:43 +0200

    Add Open Sans Condensed font

--- src/components/menuItem/menuItem.js
+++ src/components/menuItem/menuItem.js
@@ -10,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size }) => {
       className={`menu-item ${size}`}
     >
       <div className="content">
-        <h1 className="title">{ title }</h1>
+        <h1 className="title">{ title.toUpperCase() }</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
