04b8104929bfb44ae8e5eafc00438b02f875fe0d
eneax <19818937+eneax@users.noreply.github.com>
2019-07-23 11:19:24 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css';
 import Home from './pages/home/home'
 
 function App() {
   return (
     <div>
-      <Home />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
+
+const Hats = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+)
 
 export default App;
