74dfce662c5d7393d06f315ff56dabd28b034374
eneax <19818937+eneax@users.noreply.github.com>
2019-07-23 12:45:21 +0200

    Use withRouter HOC in menuItem

--- src/components/menuItem/menuItem.js
+++ src/components/menuItem/menuItem.js
@@ -1,17 +1,30 @@
-import React from "react";
-import "./menuItem.scss";
+import React from 'react';
+import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+import './menuItem.scss';
+
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <div 
+    className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
-      className="background-image"
+      className='background-image'
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
+
+/*
+* withRouter() 
+
+- it's a higher-order component (HOC)
+- it return a MenuItem component which has access to location, match, history props
+
+*/
