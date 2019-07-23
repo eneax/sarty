f9c9bab190f276e43ebde02274b3ee86cecaa20f
eneax <19818937+eneax@users.noreply.github.com>
2019-07-23 12:44:07 +0200

    Refactor menuList using the spread operator

--- src/components/menuList/menuList.js
+++ src/components/menuList/menuList.js
@@ -12,28 +12,33 @@ class MenuList extends React.Component {
           id: 1,
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          linkUrl: 'hats',
         },
         {
           id: 2,
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          linkUrl: '',
         },
         {
           id: 3,
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          linkUrl: '',
         },
         {
           id: 4,
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
+          linkUrl: '',
         },
         {
           id: 5,
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
+          linkUrl: '',
         },
       ]
     }
@@ -43,12 +48,10 @@ class MenuList extends React.Component {
     return (
       <div className="menuList">
         {
-          this.state.sections.map(({ id, title, imageUrl, size }) => (
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
             <MenuItem 
               key={id}
-              title={title}
-              imageUrl={imageUrl}
-              size={size}
+              {...otherSectionProps}
             />
           ))
         }
