fecc42ad90aa226d71b9f3f1c497d6b0128730fc
eneax <19818937+eneax@users.noreply.github.com>
2019-07-24 13:07:49 +0200

    Add navbar to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
+import Navbar from './components/navbar/navbar'
 import Home from './pages/home/home'
 import Shop from './pages/shop/shop'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/shop' component={Shop} />
