a7cc5a74879fd308f430782b660772d1bddd7764
eneax <19818937+eneax@users.noreply.github.com>
2019-07-24 13:07:17 +0200

    Create navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './navbar.scss'
+import { ReactComponent as Logo } from '../../images/crown.svg'
+
+const Navbar = () => (
+  <div className='navbar'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        Shop
+      </Link>
+      <Link className='option' to='/contact'>
+        Contact
+      </Link>
+    </div>
+  </div>
+)
+
+export default Navbar
