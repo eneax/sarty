e633d22bcd025b35a3f3eb69f16a2dc8ef8bbc0d
eneax <19818937+eneax@users.noreply.github.com>
2019-07-25 13:11:38 +0200

    Make app aware or auth state changes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,55 @@ import Home from './pages/home/home'
 import Shop from './pages/shop/shop'
 import Sign from './pages/sign/sign'
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signIn' component={Sign} />
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({
+        currentUser: user
+
+      })
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    // when the component un-mounts we want to close the subscription
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signIn' component={Sign} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+/*
+* auth.onAuthStateChanged()
+
+- onAuthStateChanged() is a method on the auth library
+- we use it to keep track of the user state (if he/she is logged in or not)
+
+*/
