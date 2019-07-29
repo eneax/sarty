f5425e37ee6fda4fcb3fa06312b9bbad62363d7f
eneax <19818937+eneax@users.noreply.github.com>
2019-07-29 13:28:05 +0200

    Create user reducer

---
+++ src/redux/user/userReducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+// INITIAL_STATE is the default value of state
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {  // action.type is a string
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload
+      }
+    default:
+      return state; // if no action matches
+  }
+}
+
+
+export default userReducer
