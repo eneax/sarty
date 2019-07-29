f47cb0ef4792d2120a19a5810b6fb3bb038af10f
eneax <19818937+eneax@users.noreply.github.com>
2019-07-29 13:27:17 +0200

    Install redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
+
+
+
+
+
+/* 
+* Provider
+
+- it's a giant object that makes the store accessible everywhere in our app
+- it needs to wrap the entire app
+*/
