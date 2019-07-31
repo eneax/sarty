0f792e5eb0b6268cad398591673df6eec17ff8bf
eneax <19818937+eneax@users.noreply.github.com>
2019-07-31 16:29:36 +0200

    Add custom button and hover effect to collectionItem

--- src/components/collectionItem/collectionItem.js
+++ src/components/collectionItem/collectionItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './collectionItem.scss'
+import Button from '../button/button'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -14,6 +15,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <Button inverted>Add to cart</Button>
   </div>
 )
 
