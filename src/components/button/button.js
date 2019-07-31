c0e31b26d4a5219595bcbb5144dbce3b3848121c
eneax <19818937+eneax@users.noreply.github.com>
2019-07-31 16:28:27 +0200

    Add inverted styles to custom button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 import './button.scss'
 
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
   <button 
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`
+      ${inverted ? 'inverted' : ''}
+      ${isGoogleSignIn ? 'google-sign-in' : ''}
+      custom-button
+    `}
     {...otherProps}
   >
     {children}
