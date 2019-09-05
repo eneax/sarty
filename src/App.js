3c73103f013cc5380b9c74d09ec621d9cfe6981e
eneax <19818937+eneax@users.noreply.github.com>
2019-09-05 20:14:54 +0200

    Restart project

--- src/App.js
+++ src/App.js
@@ -1,105 +1,12 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/userActions'
-
 import './App.css';
-import Navbar from './components/navbar/navbar'
-import Home from './pages/home/home'
-import Shop from './pages/shop/shop'
-import Sign from './pages/sign/sign'
 
-
-class App extends React.Component {
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      // if userAuth is not null (run code only if user is signing in) 
-      if (userAuth) {
-        // if a document exists, get back the userRef otherwise make a new document
-        const userRef = await createUserProfileDocument(userAuth)
-
-        // listen to userRef for any changes to the user data and set the state to the current data
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({ // setCurrentUser = this.setState
-            id: snapShot.id,
-            ...snapShot.data()
-          })
-        })
-      } else { // if user signs out
-        setCurrentUser(userAuth)
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    // when the component un-mounts we want to close the subscription
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/shop' component={Shop} />
-          <Route exact path='/signIn' render={
-            () => this.props.currentUser 
-              ? <Redirect to='/' />
-              : <Sign />
-          } />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
-
-
-
-
-
-/*
-* auth.onAuthStateChanged()
-
-- onAuthStateChanged() is a method on the auth library
-- we use it to keep track of the user state (if he/she is logged in or not)
-*/
-
-
-
-/* 
-* mapDispatchToProps()
-
-- it's a function that gets a dispatch property
-- it returns an object, where the prop name will be whatever props we pass in, 
-  that dispatches the new action that we are trying to pass (setCurrentUser)
-- if it is null, it means that we don't need to get any state (we are only setting it)
-- It only sets the value of currentUser, while navbar.js get the value and displays the view
-*/
-
-
-
-/*
-* dispatch
-
-- it is a way for redux to know that whatever object you are passing to dispatch, 
-  it's going to be an action object that dispatch will pass to the reducer
-- we basically invoke setCurrentUser() with the user that will be used as a payload
-- thanks to dispatch, we don't need anymore constructor, super, this.state and this.setState
-*/
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-
 import './index.css';
 import App from './App';
-import store from './redux/store'
 
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
-
-
-
-
-
-/* 
-* Provider
-
-- it's a giant object that makes the store accessible everywhere in our app
-- it needs to wrap the entire app
-*/
+ReactDOM.render(<App />, document.getElementById('root'));
