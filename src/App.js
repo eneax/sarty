ef95b143fbad6c933381a67e6eb8733459f01eab
eneax <19818937+eneax@users.noreply.github.com>
2019-09-05 21:54:27 +0200

    Create homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './homepage'
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Homepage />
     </div>
   );
 }
