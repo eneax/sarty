764116c101fbc56beb0da9e6b58971efe118a4df
eneax <19818937+eneax@users.noreply.github.com>
2019-09-07 10:49:32 +0200

    Refactor homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,43 +1,13 @@
 import React from 'react'
+
 import './homepage.scss'
+import MenuList from '../../components/menuList/menuList'
+
 
 const Home = () => {
   return (
     <div className='homepage'>
-      <div className="menuList">
-        
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">WOMEN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">MEN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-      </div>
+      <MenuList />
     </div>
   )
 }
