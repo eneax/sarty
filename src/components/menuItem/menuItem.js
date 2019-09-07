4616aba0b7295c8c822ac64b203699ecbaa9be91
eneax <19818937+eneax@users.noreply.github.com>
2019-09-07 11:08:09 +0200

    Add MenuItem hover effect

--- src/components/menuItem/menuItem.js
+++ src/components/menuItem/menuItem.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import './menuItem.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
-  return (
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
     <div
+      className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
-      className={`menu-item ${size}`}
-    >
-      <div className="content">
-        <h1 className="title">{title}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-  )
-}
+  </div>
+);
 
 export default MenuItem
