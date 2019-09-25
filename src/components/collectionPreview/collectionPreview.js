678964543fdad7f8e63d2b5264074698cf94b9f4
eneax <19818937+eneax@users.noreply.github.com>
2019-09-25 16:42:10 +0200

    Use CollectionItem in CollectionPreview

--- src/components/collectionPreview/collectionPreview.js
+++ src/components/collectionPreview/collectionPreview.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
 import './collectionPreview.scss'
+import CollectionItem from '../collectionItem/collectionItem'
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.filter((item, i) => i < 4).map((item) => ( 
-          <div key={item.id}>
-            {item.name}
-          </div>
-        ))}
+        {
+          items
+            .filter((item, i) => i < 4)
+            .map(({id, ...otherItemProps}) => ( 
+              <CollectionItem 
+                key={id}
+                {...otherItemProps}
+              />
+            ))
+        }
       </div>
     </div>
   )
