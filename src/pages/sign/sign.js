b17d43ee1e1ccb10d4339a2a777fcbf0fef76650
eneax <19818937+eneax@users.noreply.github.com>
2019-09-25 21:48:10 +0200

    Create Sign page

---
+++ src/pages/sign/sign.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './sign.scss'
+import SignIn from '../../components/signIn/signIn'
+
+const Sign = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  )
+}
+
+export default Sign
