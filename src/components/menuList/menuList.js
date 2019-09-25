d983696104c001c6e5f14652fb5bdd6dd7adde57
eneax <19818937+eneax@users.noreply.github.com>
2019-09-25 14:49:30 +0200

    Use spread operator in menuList

--- src/components/menuList/menuList.js
+++ src/components/menuList/menuList.js
@@ -14,28 +14,33 @@ class MenuList extends React.Component {
           id: 1,
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          linkUrl: 'hats',
         },
         {
           id: 2,
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          linkUrl: '',
         },
         {
           id: 3,
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          linkUrl: '',
         },
         {
           id: 4,
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
+          linkUrl: '',
         },
         {
           id: 5,
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
+          linkUrl: '',
         }
       ]
     }
@@ -43,13 +48,8 @@ class MenuList extends React.Component {
   render() {
     return (
       <div className='menu-list'>
-        {this.state.sections.map(({id, title, imageUrl, size}) => (
-          <MenuItem 
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     )
