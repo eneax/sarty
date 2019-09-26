c080c63cf275b3a77458ddbe71ce4ed9fb1be08d
eneax <19818937+eneax@users.noreply.github.com>
2019-09-26 11:41:45 +0200

    Add google-sign-in styles to custom btn

--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -59,7 +59,7 @@ class SignIn extends React.Component {
           <CustomButton type='submit'>
               Sign In
           </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
             Sign In With Google
           </CustomButton>
         </form>
