b09c595912aab67594c194b2a7007a72b858b80e
eneax <19818937+eneax@users.noreply.github.com>
2019-09-27 17:29:12 +0200

    Use mapDispatchToProps in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
+import { setCurrentUser } from './redux/user/userActions'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import './App.css'
 import Header from './components/header/header'
@@ -10,35 +12,24 @@ import Sign from './pages/sign/sign'
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   // Initial subscription is null
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
     // Store user data in our app
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           })
         })
       } else {
-        this.setState({
-          currentUser: userAuth // null
-        })
+        setCurrentUser(userAuth) // null
       }
     })
   }
@@ -62,4 +53,23 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
+
+
+
+
+
+/*
+* connect(null, mapDispatchToProps)
+- null because we don't need mapStateToProps, since App doesn't need currentUser
+- mapDispatchToProps gets a dispatch property and returns an object where the prop name will be whatever prop
+  we want to pass in that dispatches the new action that we are trying to pass
+- dispatch() takes an action object an passes it to every reducer
+
+
+* setCurrentUser replaces this.setState()
+*/ 
