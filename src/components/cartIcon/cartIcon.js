61944ccb3ac720a861a1e25a4e277782750dfdf2
eneax <19818937+eneax@users.noreply.github.com>
2019-09-28 21:43:12 +0200

    Use createStructuredSelector in cartIcon

--- src/components/cartIcon/cartIcon.js
+++ src/components/cartIcon/cartIcon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { toggleCartHidden } from '../../redux/cart/cartActions'
 import { selectCartItemsCount } from '../../redux/cart/cartSelectors'
@@ -14,8 +15,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 })
 
 const mapDispatchToProps = (dispatch) => ({
