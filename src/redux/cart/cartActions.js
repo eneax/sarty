07b433c9c38e9c6b590e86b998e546a60b43e6f0
eneax <19818937+eneax@users.noreply.github.com>
2019-09-28 14:02:48 +0200

    Add addItem in cartActions

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,5 +1,11 @@
 import { CartActionTypes } from './cartTypes'
 
+
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
 })
