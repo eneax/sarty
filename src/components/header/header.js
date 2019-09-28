a479a84b7170161ef2e9197e0518809e3874342d
eneax <19818937+eneax@users.noreply.github.com>
2019-09-28 21:43:33 +0200

    Use createStructuredSelector in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { auth } from '../../firebase/firebase.utils'
+import { selectCurrentUser } from '../../redux/user/userSelectors'
+import { selectCartHidden } from '../../redux/cart/cartSelectors'
 import './header.scss'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import CartIcon from '../cartIcon/cartIcon'
@@ -33,9 +36,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 )
 
-const mapStateToProps = ({ user: { currentUser}, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
 })
 
 export default connect(mapStateToProps)(Header)
