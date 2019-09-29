28518122b6e62fa20d26032bd86cbdce7fbb7c61
eneax <19818937+eneax@users.noreply.github.com>
2019-09-29 16:39:45 +0200

    Refactor ShopPage as a functional component

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,35 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-import SHOP_DATA from '../../data/shopData'
+import { selectCollections } from '../../redux/shop/shopSelectors'
 
 import CollectionPreview from '../../components/collectionPreview/collectionPreview'
 
 
-export default class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-
-    return (
-      <div className='shop-page'>
-        <h1>Shop Page</h1>
-        
-        {
-          collections.map(({ id, ...otherCollectionProps}) => (
-            <CollectionPreview 
-              key={id}
-              {...otherCollectionProps}
-            />
-          ))
-        }
-      </div>
-    )
-  }
+const ShopPage = ({ collections }) => {
+  return (
+    <div className='shop-page'>
+      <h1>Shop Page</h1>
+      
+      {
+        collections.map(({ id, ...otherCollectionProps}) => (
+          <CollectionPreview 
+            key={id}
+            {...otherCollectionProps}
+          />
+        ))
+      }
+    </div>
+  )
 }
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage)
