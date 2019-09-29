20b954f71ecd8571b30b413b6203599fb6256cbd
eneax <19818937+eneax@users.noreply.github.com>
2019-09-29 10:48:02 +0200

    Create checkout header

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,14 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cartSelectors'
 
 import './checkout.scss'
 
 
-const Checkout = () => {
+const Checkout = ({ cartItems, total }) => {
   return (
-    <div>
-      Checkout page
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+
+      {
+        cartItems.map((cartItem) => (
+          cartItem.name
+        ))
+      }
+
+      <div className='total'>
+        <span>TOTAL: ${total}</span>
+      </div>
     </div>
   )
 }
 
-export default Checkout
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(Checkout)
