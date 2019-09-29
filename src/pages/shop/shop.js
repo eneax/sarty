ec42c93d1abda355cf3c65a5e2f1d9ebba3e8f77
eneax <19818937+eneax@users.noreply.github.com>
2019-09-29 16:52:03 +0200

    Refactor shop page and import CollectionsOverview

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,31 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
-import { selectCollections } from '../../redux/shop/shopSelectors'
+import CollectionsOverview from '../../components/collectionsOverview/collectionsOverview'
 
-import CollectionPreview from '../../components/collectionPreview/collectionPreview'
-
-
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className='shop-page'>
       <h1>Shop Page</h1>
       
-      {
-        collections.map(({ id, ...otherCollectionProps}) => (
-          <CollectionPreview 
-            key={id}
-            {...otherCollectionProps}
-          />
-        ))
-      }
+      <CollectionsOverview />
     </div>
   )
 }
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage)
+export default ShopPage
