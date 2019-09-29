bc352975421ae614e2eae57728815ecf26980326
eneax <19818937+eneax@users.noreply.github.com>
2019-09-29 11:54:37 +0200

    Create clearItemFromCart action

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -7,5 +7,10 @@ export const toggleCartHidden = () => ({
 
 export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
-  payload: item
+  payload: item // item we want to add
+})
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item // item we want to clear
 })
