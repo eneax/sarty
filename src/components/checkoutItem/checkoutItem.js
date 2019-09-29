3cd2b1255d490babe4ad9fb2e9482d253d05a6dd
eneax <19818937+eneax@users.noreply.github.com>
2019-09-29 12:02:25 +0200

    Add static arrows

--- src/components/checkoutItem/checkoutItem.js
+++ src/components/checkoutItem/checkoutItem.js
@@ -15,7 +15,11 @@ const CheckoutItem = ({ cartItem, clearItemFromCart }) => {
         <img src={imageUrl} alt='checkout item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow'>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow'>&#10095;</div>
+      </span>
       <span className='price'>{price}</span>
       <div 
         className='remove-button'
