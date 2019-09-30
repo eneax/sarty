b2f6cf06b1148be203c4ff1db91ba8ffd7309054
eneax <19818937+eneax@users.noreply.github.com>
2019-09-30 11:31:22 +0200

    Normalize shop data

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,18 +1,6 @@
 import { createSelector } from 'reselect'
 
 
-/*
-  map the string value (that we get from the url parameter) 
-  to the respective id number (that we get from the shop data)
-*/
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = (state) => state.shop
 
 export const selectCollections = createSelector(
@@ -22,6 +10,5 @@ export const selectCollections = createSelector(
 
 export const selectCollection = (collectionUrlParam) => createSelector(
   [selectCollections],
-  // find collection.id that matches the url parameter of the collection id map 
-  (collections) => collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+  (collections) => collections[collectionUrlParam]
 )
