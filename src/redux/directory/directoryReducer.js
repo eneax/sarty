9dbcaec51c0fd07bdbdafa63e4581f7af324711e
eneax <19818937+eneax@users.noreply.github.com>
2019-09-30 11:21:22 +0200

    Link directory to collection categories

--- src/redux/directory/directoryReducer.js
+++ src/redux/directory/directoryReducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
       id: 1,
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'hats',
+      linkUrl: 'shop/hats',
     },
     {
       id: 2,
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      linkUrl: '',
+      linkUrl: 'shop/jackets',
     },
     {
       id: 3,
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      linkUrl: '',
+      linkUrl: 'shop/sneakers',
     },
     {
       id: 4,
       title: 'womens',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
-      linkUrl: '',
+      linkUrl: 'shop/womens',
     },
     {
       id: 5,
       title: 'mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
-      linkUrl: '',
+      linkUrl: 'shop/mens',
     }
   ]
 }
