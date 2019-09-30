da5ab2985d7c7d0102cea6fe4f70a004888d4412
eneax <19818937+eneax@users.noreply.github.com>
2019-09-30 11:08:49 +0200

    Create selectCollection selector

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,8 +1,27 @@
 import { createSelector } from 'reselect'
 
+
+/*
+  map the string value (that we get from the url parameter) 
+  to the respective id number (that we get from the shop data)
+*/
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+}
+
 const selectShop = (state) => state.shop
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+)
+
+export const selectCollection = (collectionUrlParam) => createSelector(
+  [selectCollections],
+  // find collection.id that matches the url parameter of the collection id map 
+  (collections) => collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 )
