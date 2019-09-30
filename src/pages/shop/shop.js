c600b43eddb817fb2e484a83c1d83e80b002bdb2
eneax <19818937+eneax@users.noreply.github.com>
2019-09-30 11:10:37 +0200

    Import collection page into shop page

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collectionsOverview/collectionsOverview'
+import CollectionPage from '../collection/collection'
 
-const ShopPage = () => {
+
+const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
-      <h1>Shop Page</h1>
-      
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 }
