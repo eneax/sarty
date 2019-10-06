ca59b4201e34f8341294e69cb9824bb2a7ff0e51
eneax <19818937+eneax@users.noreply.github.com>
2019-10-06 17:53:55 +0200

    Add onClick to TitleContainer in CollectionPreview

--- src/components/collectionPreview.js
+++ src/components/collectionPreview.js
@@ -1,31 +1,28 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import CollectionItem from './collectionItem'
 
 import {
   CollectionPreviewContainer,
   TitleContainer,
   PreviewContainer
 } from './styles/collectionPreviewStyles'
-import CollectionItem from './collectionItem'
 
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <CollectionPreviewContainer>
-      <TitleContainer>{title.toUpperCase()}</TitleContainer>
-      <PreviewContainer>
-        {
-          items
-            .filter((item, i) => i < 4)
-            .map((item) => ( 
-              <CollectionItem 
-                key={item.id}
-                item={item}
-              />
-            ))
-        }
-      </PreviewContainer>
-    </CollectionPreviewContainer>
-  )
-}
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
+    <PreviewContainer>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </PreviewContainer>
+  </CollectionPreviewContainer>
+)
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
