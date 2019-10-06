f7814a5981ea618d5e0d934b0a7cc5e9aea863c0
eneax <19818937+eneax@users.noreply.github.com>
2019-10-06 17:45:36 +0200

    Refactor WithSpinner component

--- src/components/withSpinner.js
+++ src/components/withSpinner.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { SpinnerContainer, SpinnerOverlay} from './styles/withSpinnerStyles'
+import { SpinnerContainer, SpinnerOverlay } from './styles/withSpinnerStyles'
 
 
-const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
-  return (
-    isLoading
-      ? <SpinnerOverlay>
-          <SpinnerContainer />
-        </SpinnerOverlay>
-      : <WrappedComponent { ...otherProps } />
-  )
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return (
+      isLoading 
+        ? <SpinnerOverlay>
+            <SpinnerContainer />
+          </SpinnerOverlay>
+        : <WrappedComponent {...otherProps} />
+    )
+  }
+
+  return Spinner
 }
 
 export default WithSpinner
