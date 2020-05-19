a109c874e9671fdaa54bffc1b2ea4f8bea6bf8fe
eneax <19818937+eneax@users.noreply.github.com>
2020-05-19 15:08:22 +0200

    Use SECTIONS_DATA in Directory

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import SECTIONS_DATA from "./sectionsData";
 import './directoryStyles.scss'
 import MenuItem from '../menuItem/menuItem'
 
@@ -7,40 +9,7 @@ class Directory extends React.Component {
     super();
 
     this.state = {
-      sections : [
-        {
-          id: 1,
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          linkUrl: 'shop/hats'
-        },
-        {
-          id: 2,
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          linkUrl: 'shop/jackets'
-        },
-        {
-          id: 3,
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          id: 4,
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          linkUrl: 'shop/womens',
-          size: 'large'
-        },
-        {
-          title: 'mens',
-          id: 5,
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          linkUrl: 'shop/mens',
-          size: 'large'
-        }
-      ]
+      sections : SECTIONS_DATA,
     }
   }
 
