c677300d5dbc57f20404896d228aa152b68fbaaa
eneax <19818937+eneax@users.noreply.github.com>
2020-05-19 17:21:10 +0200

    Create auth page

---
+++ src/pages/auth/auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/signIn/signIn';
+
+const Auth = () => (
+  <div className='auth'>
+    <h1>Authentication</h1>
+
+    <SignIn />
+  </div>
+);
+
+export default Auth;
