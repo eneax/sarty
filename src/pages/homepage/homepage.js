4b4143f41d9febdae264865d6c3136b6a6ceb44e
eneax <19818937+eneax@users.noreply.github.com>
2020-05-19 10:31:37 +0200

    Refactor homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,41 +1,11 @@
 import React from 'react';
 import './homepageStyles.scss'
+import Directory from '../../components/directory/directory'
 
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <div className="menuList">
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menuItem">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
