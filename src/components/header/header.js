b6620f197c2b22cb7812878d75fc93a44e62b457
eneax <19818937+eneax@users.noreply.github.com>
2020-05-21 17:55:29 +0200

    Connect header component to redux

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './headerStyles.scss';
-import {ReactComponent as Logo} from '../../assets/logo.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 import { auth } from '../../firebase/firebase.utils'
 
@@ -23,4 +24,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header
+const mapStateToProps = ( state ) => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
