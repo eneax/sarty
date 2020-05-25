575069c9708dd0a24d333bc5245ea395d7175e6c
eneax <19818937+eneax@users.noreply.github.com>
2020-05-25 17:38:57 +0200

    Use cartSelectors in CartIcon

--- src/components/cartIcon/cartIcon.js
+++ src/components/cartIcon/cartIcon.js
@@ -3,17 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cartActions';
+import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cartIconStyles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cartIcon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shoppingIcon" />
-    <span className="itemCount">0</span>
+    <span className="itemCount">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
@@ -23,4 +28,4 @@ CartIcon.propTypes = {
   toggleCartHidden: PropTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
