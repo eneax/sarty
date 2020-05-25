7d21de3e4c025e090b18436688e91cd40ed8dcac
eneax <19818937+eneax@users.noreply.github.com>
2020-05-25 15:39:44 +0200

    Add CartItem to CartDropdown and connect to redux

--- src/components/cartDropdown/cartDropdown.js
+++ src/components/cartDropdown/cartDropdown.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import './cartDropdownStyles.scss';
+import CartItem from '../cartItem/cartItem';
 import CustomButton from '../customButton/customButton';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cartDropdown">
-    <div className="cartItems">items</div>
+    <div className="cartItems">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems,
+});
+
+CartDropdown.propTypes = {
+  cartItems: PropTypes.array,
+};
+
+export default connect(mapStateToProps)(CartDropdown);
