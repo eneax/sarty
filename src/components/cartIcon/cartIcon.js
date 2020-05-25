a4525f535d383fc64c64e2cced17b859b08e4c0a
eneax <19818937+eneax@users.noreply.github.com>
2020-05-25 10:10:09 +0200

    Dispatch toggleCart action from cartIcon

--- src/components/cartIcon/cartIcon.js
+++ src/components/cartIcon/cartIcon.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cartActions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cartIconStyles.scss';
 
-const CartIcon = () => (
-  <div className="cartIcon">
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cartIcon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shoppingIcon" />
     <span className="itemCount">0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+CartIcon.propTypes = {
+  toggleCartHidden: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(CartIcon);
