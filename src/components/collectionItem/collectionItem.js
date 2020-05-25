eef6d23aa46486e6a91ab93e2c754643ab45ecc0
eneax <19818937+eneax@users.noreply.github.com>
2020-05-25 11:50:49 +0200

    Add addItem functionality to CollectionItem component

--- src/components/collectionItem/collectionItem.js
+++ src/components/collectionItem/collectionItem.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cartActions';
 
 import './collectionItem.scss';
 import CustomButton from '../customButton/customButton';
 
-const CollectionItem = ({ imageUrl, name, price }) => (
-  <div className="collectionItem">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
 
-    <div className="collectionFooter">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+  return (
+    <div className="collectionItem">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+
+      <div className="collectionFooter">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to cart
+      </CustomButton>
     </div>
-
-    <CustomButton inverted className="customButton">
-      Add to cart
-    </CustomButton>
-  </div>
-);
-
-CollectionItem.propTypes = {
-  imageUrl: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
+  );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+});
+
+CollectionItem.propTypes = {
+  item: PropTypes.shape({
+    imageUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+  addItem: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collectionPreview/collectionPreview.js
+++ src/components/collectionPreview/collectionPreview.js
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4) // get only 4 items as a preview
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
