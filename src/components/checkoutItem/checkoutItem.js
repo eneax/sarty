982918945d768f55c0b218778c034f1b366d7628
eneax <19818937+eneax@users.noreply.github.com>
2020-05-26 12:13:12 +0200

    Add arrows to CheckoutItem

--- src/components/checkoutItem/checkoutItem.js
+++ src/components/checkoutItem/checkoutItem.js
@@ -16,7 +16,13 @@ const CheckoutItem = ({ item, removeItemFromCart }) => {
       </div>
 
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+
+      <span className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow">&#10095;</div>
+      </span>
+
       <span className="price">{price}</span>
 
       <div className="removeButton" onClick={() => removeItemFromCart(item)}>
