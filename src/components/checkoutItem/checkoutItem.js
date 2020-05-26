fc2b57081b78e169d723a12671bd3a6a985d81c8
eneax <19818937+eneax@users.noreply.github.com>
2020-05-26 12:03:55 +0200

    Use removeItemFromCart in CheckoutItem

--- src/components/checkoutItem/checkoutItem.js
+++ src/components/checkoutItem/checkoutItem.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { removeItemFromCart } from '../../redux/cart/cartActions';
 
 import './checkoutItemStyles.scss';
 
-const CheckoutItem = ({ item: { imageUrl, name, quantity, price } }) => (
-  <div className="checkoutItem">
-    <div className="imageContainer">
-      <img src={imageUrl} alt={`${name} pic`} />
+const CheckoutItem = ({ item, removeItemFromCart }) => {
+  const { imageUrl, name, quantity, price } = item;
+
+  return (
+    <div className="checkoutItem">
+      <div className="imageContainer">
+        <img src={imageUrl} alt={`${name} pic`} />
+      </div>
+
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+
+      <div className="removeButton" onClick={() => removeItemFromCart(item)}>
+        &#10007;
+      </div>
     </div>
+  );
+};
 
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-
-    <div className="removeButton">&#10007;</div>
-  </div>
-);
+const mapDispatchToProps = dispatch => ({
+  removeItemFromCart: item => dispatch(removeItemFromCart(item)),
+});
 
 CheckoutItem.propTypes = {
   item: PropTypes.shape({
@@ -24,6 +37,7 @@ CheckoutItem.propTypes = {
     quantity: PropTypes.number.isRequired,
     price: PropTypes.number.isRequired,
   }).isRequired,
+  removeItemFromCart: PropTypes.func.isRequired,
 };
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
