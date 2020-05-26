413e83d023a5db7ac5208ff2d56048c43b54bca6
eneax <19818937+eneax@users.noreply.github.com>
2020-05-26 11:24:05 +0200

    Toggle CartDropdown when button is clicked

--- src/components/cartDropdown/cartDropdown.js
+++ src/components/cartDropdown/cartDropdown.js
@@ -5,12 +5,13 @@ import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems } from '../../redux/cart/cartSelectors';
+import { toggleCartHidden } from '../../redux/cart/cartActions';
 
 import './cartDropdownStyles.scss';
 import CartItem from '../cartItem/cartItem';
 import CustomButton from '../customButton/customButton';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cartDropdown">
     <div className="cartItems">
       {cartItems.length ? (
@@ -21,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
@@ -34,6 +40,7 @@ const mapStateToProps = createStructuredSelector({
 CartDropdown.propTypes = {
   cartItems: PropTypes.array,
   history: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
