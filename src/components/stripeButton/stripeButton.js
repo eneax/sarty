24da3063830a192ce42ba46596650b001a9c3699
eneax <19818937+eneax@users.noreply.github.com>
2020-05-27 15:51:54 +0200

    Create StripeCheckoutButton component

---
+++ src/components/stripeButton/stripeButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100; // price in cents
+  const publishableKey = 'pk_test_NfZKonMxPTdVX8vxFNUcyRpf00uXFC0iiI';
+
+  // We don't process any payment
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      panelLabel="Pay Now"
+      name="Sarty Inc."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      currency="EUR"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+StripeCheckoutButton.propTypes = {
+  price: PropTypes.number.isRequired,
+};
+
+export default StripeCheckoutButton;
