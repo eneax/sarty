3711b66ffd3fe01e56a2d3fd9c7699f63cf23374
eneax <19818937+eneax@users.noreply.github.com>
2020-05-27 12:35:38 +0200

    Config routing for shop and collection page

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 
+import CollectionPage from '../collection/collection';
 import CollectionsOverview from '../../components/collectionsOverview/collectionsOverview';
 
-const Shop = () => (
+const Shop = ({ match }) => (
   <div className="shopPage">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
+
+Shop.propTypes = {
+  match: PropTypes.shape({
+    isExact: PropTypes.bool.isRequired,
+    params: PropTypes.object.isRequired,
+    path: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Shop;
