705e916586e6622b2622aa46798111db6e6e6c9c
eneax <19818937+eneax@users.noreply.github.com>
2020-05-27 13:00:58 +0200

    Convert shop data in object type

--- src/components/collectionsOverview/collectionsOverview.js
+++ src/components/collectionsOverview/collectionsOverview.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shopSelectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shopSelectors';
 
 import './collectionsOverviewStyles.scss';
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 CollectionsOverview.propTypes = {
